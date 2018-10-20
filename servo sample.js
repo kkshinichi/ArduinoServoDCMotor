794798dc9b2e3fc18df5b7b07a19531214942138
Theo Leonard P. Tejada <35455761+kkshinichi@users.noreply.github.com>
2018-10-20 16:11:40 +0800

    Added Button Support for relay :3

--- servo sample.js
+++ servo sample.js
@@ -5,12 +5,27 @@ var board = new five.Board({
 });
 
 board.on('ready', function() {
+	var up = new five.Button(4);
+	var down = new five.Button(5);
 	var servo = new five.Servo(1);
 	this.repl.inject({
-    servo: servo
+    servo: servo,
+    button: up,
+    button: down
   });
-	servo.center();
-	board.wait(2000, function() {
+
+	up.on("down", function() {
+		console.log("Going Down.");
 		servo.min();
 	});
+	down.on("down", function() {
+		console.log("Going Up");
+		servo.center();
+	});
+
+
+	// servo.center();
+	// board.wait(2000, function() {
+	// 	servo.min();
+	// });
 });
